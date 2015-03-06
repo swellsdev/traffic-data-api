81b7589b99ba8d7a44d04059400d0441873e1205
Scotta <--global>
2015-03-07 10:24:21 +1100

    added app

---
+++ app.js
@@ -0,0 +1,17 @@
+var config  = require('./config');
+var express = require('express');
+
+var app = express();
+
+app.set('port', config.port);
+
+// health check
+app.get('/', function (req, res) {
+  res.send('Service OK');
+});
+
+var server = app.listen(config.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ config.js
@@ -0,0 +1,15 @@
+function loadEnv(key) {
+  var environmentVariable = process.env[key];
+
+  if (environmentVariable) {
+    return environmentVariable;
+  }
+  console.err('Environment variable', key, 'not set. Exiting');
+  process.exit(1); // fail fast
+}
+
+module.exports = {
+  username:      loadEnv('MONGO_USERNAME'),
+  password:      loadEnv('MONGO_PASSWORD'),
+  port:          loadEnv('PORT')
+}
