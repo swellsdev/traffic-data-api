d17273c1b8f8bca659677d51d756517924650650
Scotta <--global>
2015-03-07 14:04:04 +1100

    got location request working

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 var config  = require('./config')
 var express = require('express')
-var mongoose = require('mongoose');
-
+var mongo = require('mongodb')
+var monk = require('monk')
 var app = express()
-
 app.set('port', config.port);
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var mongoURI = "mongodb://"+config.username+":"+config.password
+	+"@ds051851.mongolab.com:51851/vicroads-segment-speed";
+
+MongoClient.connect(mongoURI, function(err, db) {
+  	assert.equal(null, err);
+  	console.log("Connected correctly to server");
+  	console.log(db.databaseName);
+  	GetLocations(db, function(result){
+  		locations = result;
+  		//db.close();
+  	});
+});
 
 // health check
 app.get('/', function (req, res) {
-  res.send('Service OK');
+  	res.send('Service OK');
+});
+
+// get locations
+app.get('/locations', function (req, res) {
+  res.send(locations);
 });
 
 var server = app.listen(config.port, function () {
@@ -16,3 +35,19 @@ var server = app.listen(config.port, function () {
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+function GetLocations(db,callback){
+	var collection = db.collection('locations');
+	collection.find({}).toArray(function(err,result){
+		callback(result);
+	});
+}
+
+function GetLocationCSV(surveyID,db){
+	var collection = db.collection('data');
+	collection.findOne({"NB_LOCATION_TRAFFIC_SURVEY":surveyID},{"NB_LOCATION_TRAFFIC_SURVEY":true,"NB_GPS_LATITUDE":true,"NB_GPS_LONGITUDE":true},function(err,item){
+		console.log(item.NB_LOCATION_TRAFFIC_SURVEY+","
+			+item.NB_GPS_LATITUDE+","
+			+item.NB_GPS_LONGITUDE);
+	});
+}
