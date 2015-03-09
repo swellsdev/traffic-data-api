08e03c3a0fd14000ae63344c348e9abc903c2ded
Scotta <--global>
2015-03-09 19:52:41 +1100

    fixing access

--- app.js
+++ app.js
@@ -12,6 +12,27 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(cors())
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'yourpeakhour.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.set('port', config.port);
 
 var MongoClient = require('mongodb').MongoClient
